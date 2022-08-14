068c81e39a3cefbc00f49eb8d9727d9a52320bf9
unknown <epicfisher@users.noreply.github.com>
2022-08-14 13:02:13 +0100

    fix: Newlines, and fix argument edit ordering

--- start.sh
+++ start.sh
@@ -3,27 +3,27 @@ printf '%s\n' "$PWD"/music/*.ogg >> playlist.txt
 printf '%s\n' "$PWD"/music/*.mp3 >> playlist.txt
 
 printf 'Preparing Configuration Files...\n\n'
-export CFG_ADMIN=${CFG_ADMIN:-"icemaster@localhost"}
-export CFG_ADMIN_PASSWORD=${CFG_ADMIN_PASSWORD:-"hackme"}
 export CFG_ADMIN_USER=${CFG_ADMIN_USER:-"admin"}
+export CFG_ADMIN_PASSWORD=${CFG_ADMIN_PASSWORD:-"hackme"}
+export CFG_ADMIN=${CFG_ADMIN:-"icemaster@localhost"}
 export CFG_LOCATION=${CFG_LOCATION:-"Earth"}
 export CFG_RELAY_PASSWORD=${CFG_RELAY_PASSWORD:-"hackme"}
 export CFG_SOURCE_PASSWORD=${CFG_SOURCE_PASSWORD:-"hackme"}
 export CFG_STREAM_URL=${CFG_STREAM_URL:-"0.0.0.0"}
 # ---
 sed -i -e "s/\$PORT/$PORT/g" icecast.xml
-sed -i -e "s/\$CFG_ADMIN/$CFG_ADMIN/g" icecast.xml
-sed -i -e "s/\$CFG_ADMIN_PASSWORD/$CFG_ADMIN_PASSWORD/g" icecast.xml
 sed -i -e "s/\$CFG_ADMIN_USER/$CFG_ADMIN_USER/g" icecast.xml
+sed -i -e "s/\$CFG_ADMIN_PASSWORD/$CFG_ADMIN_PASSWORD/g" icecast.xml
+sed -i -e "s/\$CFG_ADMIN/$CFG_ADMIN/g" icecast.xml
 sed -i -e "s/\$CFG_LOCATION/$CFG_LOCATION/g" icecast.xml
 sed -i -e "s/\$CFG_RELAY_PASSWORD/$CFG_RELAY_PASSWORD/g" icecast.xml
 sed -i -e "s/\$CFG_SOURCE_PASSWORD/$CFG_SOURCE_PASSWORD/g" icecast.xml
 sed -i -e "s/\$CFG_STREAM_URL/$CFG_STREAM_URL/g" icecast.xml
 # ---
 sed -i -e "s/\$PORT/$PORT/g" ices.xml
-sed -i -e "s/\$CFG_ADMIN/$CFG_ADMIN/g" ices.xml
-sed -i -e "s/\$CFG_ADMIN_PASSWORD/$CFG_ADMIN_PASSWORD/g" ices.xml
 sed -i -e "s/\$CFG_ADMIN_USER/$CFG_ADMIN_USER/g" ices.xml
+sed -i -e "s/\$CFG_ADMIN_PASSWORD/$CFG_ADMIN_PASSWORD/g" ices.xml
+sed -i -e "s/\$CFG_ADMIN/$CFG_ADMIN/g" ices.xml
 sed -i -e "s/\$CFG_LOCATION/$CFG_LOCATION/g" ices.xml
 sed -i -e "s/\$CFG_RELAY_PASSWORD/$CFG_RELAY_PASSWORD/g" ices.xml
 sed -i -e "s/\$CFG_SOURCE_PASSWORD/$CFG_SOURCE_PASSWORD/g" ices.xml
@@ -38,7 +38,7 @@ sleep 10
 printf '\nStarting Ices2... (Audio Streamer)\n\n'
 ices2 ices.xml
 
-printf "(Now Sleeping Forever!)\n\nYour Radio Server Webpage should now be Live!\nOn your App's Dashboard, Click 'Open app' in the top right to Open your Radio's Webpage!"
+printf "(Now Sleeping Forever!)\n\nYour Radio Server Webpage should now be Live!\n\nOn your App's Dashboard, Click 'Open app' in the top right to Open your Radio's Webpage!\n\n"
 while true
 do
 	sleep 10
